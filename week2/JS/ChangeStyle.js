5024d291d575bae20221f81d428718175fbd61f7
tanbham <tanmay.bhambure@gmail.com>
2020-06-04 14:58:02 +0530

    week2

---
+++ week2/JS/ChangeStyle.js
@@ -0,0 +1,9 @@
+function open(){
+	x = document.getElementById("hemm");
+	x.innerHTML = "bye";
+	//x.className = "opened";
+}
+function close(){
+	x = document.getElementById("hemm");
+	x.className = "closed";
+}
